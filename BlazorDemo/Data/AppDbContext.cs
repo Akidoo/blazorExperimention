f047374c0c666dfbba6b157a7fdb636d433bb659
Akaash Banerjee <plunkels@gmail.com>
2024-01-18 23:36:49 +0800

    update database

---
+++ BlazorDemo/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorDemo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration configuration) //sets up class before methods can be used
+        {
+            Configuration = configuration;
+        }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DbConnectionString"));
+        
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
---
+++ BlazorDemo/Models/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorDemo.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Deadline { get; set; }
+    }
+}
